eb556fd72647960899c82df00722cd0d61ac5647
Shankar Rao <shankar.rao@gmail.com>
2021-11-06 13:15:31 +0100

    Minor reword in settings.php

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,3 +1,3 @@
 <?php
 
-$lang['add historical bookings'] = 'Enable adding of historical bookings (up to the beginning of the year or two months ago, whichever is earlier)';
+$lang['add historical bookings'] = 'Enable adding of historical bookings (to beginning of year or two months ago, whichever is earlier)';
